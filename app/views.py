31dc2ec8ff258e94a6a543a4a274c8c63dd96407
MK3247 <wafulacharles47@gmail.com>
2022-06-08 21:48:35 +0300

    create auction view

--- app/views.py
+++ app/views.py
@@ -34,13 +34,52 @@ def index(request):
         'title': 'Dashboard'
     })
 
+def create_auction(request):
+    ImageFormSet = forms.modelformset_factory(Image, form = ImageForm, extra =2)
+    if request.method == 'POST':
+        auction_form = AuctionForm(request.POST, request.FILES)
+        image_form = ImageFormSet(request.POST, request.FILES, queryset=Image.objects.none())
+
+        if auction_form.is_valid() and image_form.is_valid():
+            new_auction = auction_form.save(commit=False)
+            new_auction.creator = request.user
+            new_auction.save()
+
+            for auction_form in image_form.cleaned_data:
+                if auction_form:
+                    image = auction_form['image']
+
+                    new_image = Image(auction=new_auction, image=image)
+                    new_image.save()
+
+            return render(request, 'create_auction.html', {
+                'categories': Category.objects.all(),
+                'auction_form':AuctionForm(),
+                'image_form': ImageFormSet(queryset=Image.objects.none()),
+                'title': 'Create Auction',
+                'success': True
+            })
+
+        else:
+            return render(request, 'create_auction.html', {
+                'categories':Category.objects.all(),
+                'auction_form': AuctionForm(),
+                'image_form':ImageFormSet(queryset=Image.objects.none()),
+                'title':'Create Auction'
+            })
+    else:
+        return render(request, 'create_auction.html', {
+                'categories':Category.objects.all(),
+                'auction_form': AuctionForm(),
+                'image_form':ImageFormSet(queryset=Image.objects.none()),
+                'title':'Create Auction'
+            })
+
+            
 def active(request):
 
     return render (request, 'active.html', {})
 
-def create_auction(request):
-
-    return render(request, 'create_auction.html', {})
 
 def watch(request):
 
